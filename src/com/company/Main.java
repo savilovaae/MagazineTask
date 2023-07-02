89a14d7bc61c0ae97c5737892e5c60205d642ac2
savilovaae <117019785+savilovaae@users.noreply.github.com>
2023-07-02 22:48:44 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,32 @@
 package com.company;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+    public static void showAllProducts(Category[] allCategories) {
+        // метод для того, чтобы показать весь ассортимент магазина по категориям
+        for (int i = 0; i < allCategories.length; i++) {
+            System.out.println(String.format("Category: %s\n List of products:", allCategories[i].name,
+                    allCategories[i].getCategoryList()));
+        }
+    }
+
+    public static void main(String[] args) {
+        /* массив allCategories - двумерный и каждый элемент может быть разной длины(ступенька),
+        т.к каждая категория является массивом продуктов */
+        final List<Category> allCategories = new ArrayList<Category>(); // почему тут public(etc) нельзя использовать
+
+        Category milk = new Category().setName("milk");
+        Category meat = new Category().setName("meat");
+        Category sweets = new Category().setName("sweets");
+
+        Basket milkaBasket = new Basket();
+
+        User Kurkuma = new User().setLogin("kukuruma");
+        User Kirito = new User().setLogin("saoLover").setPassword("h_haruka");
+        User Milka = new User().setLogin("podoMilka").setPassword("the best flovor").setBasket(milkaBasket);
+        Milka.getUserBusket();
     }
 }
